00f15f1f038daf274d4a3872851141990ca2f815
azkamalaz <kamalazenag2@gmail.com>
2023-08-02 15:32:17 +0100

    sbb

---
+++ alx-low_level_programming/0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
---
+++ alx-low_level_programming/0x08-recursion/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_FILE
+#define MAIN_FILE
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
