6b6cafcba9d3d193b20b9cfce2dcb406da8bc957
zankoss <kamalazenag2@gmail.com>
2023-08-03 02:23:45 +0100

    123

--- alx-low_level_programming/0x04-more_functions_nested_loops/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_number(int n);
-#endif
--- alx-low_level_programming/0x08-recursion/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_FILE
-#define MAIN_FILE
-
-int _putchar(char c);
-void _puts_recursion(char *s);
-
-#endif
